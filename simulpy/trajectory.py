604449be4528f38d31867312da36865cfcb4196f
Arjun Sahdev <arjunsahdev13@gmail.com>
2019-07-29 14:27:59 +0530

    Add files via upload

--- simulpy/trajectory.py
+++ simulpy/trajectory.py
@@ -2,6 +2,7 @@ import numpy as np
 from simulpy import kin
 
 
+# create the matrix to plot the robot arm
 def calctrajectorymat(thetamat, robobj):
 
     i = 0
@@ -15,6 +16,7 @@ def calctrajectorymat(thetamat, robobj):
     return trajectorymat
 
 
+# create list of joint angles from initial to final position of the robot
 def calctrac(robobj, thetainit, thetafinal, nopoints):
 
     thetamat = np.zeros((nopoints + 2, robobj.jointno))
@@ -26,6 +28,21 @@ def calctrac(robobj, thetainit, thetafinal, nopoints):
         while j < robobj.jointno:
             thetamat[i][j] = thetainit[j] + i*thetafinal[j]/(nopoints+1)
             j = j + 1
+        i = i + 1
+
+    return calctrajectorymat(thetamat, robobj)
+
+
+def linetrac(robobj, pt1, pt2):
+    x = np.linspace(pt1[0], pt2[0])
+    y = np.linspace(pt1[1], pt2[1])
+    z = np.linspace(pt1[2], pt2[2])
+
+    i = 0
+    thetamat = np.zeros((len(x), 3))
+
+    while i < len(x):
+        thetamat[i] = kin.inv(robobj, [x[i], y[i], z[i]])
         i = i + 1
 
     return calctrajectorymat(thetamat, robobj)
