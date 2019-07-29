8d5c27c7f7feff71342bf44a0319301d04d18bc3
Arjun Sahdev <arjunsahdev13@gmail.com>
2019-07-29 13:16:44 +0530

    Add files via upload

--- simulpy/kin.py
+++ simulpy/kin.py
@@ -2,7 +2,10 @@ import copy
 import numpy as np
 import math
 
+pi = math.pi
 
+
+#function to perform forward kinematics of open chain robot
 def fwd(robobj, thetalist):
 
     rotmat = rotation_matrix(robobj, thetalist)
@@ -22,6 +25,7 @@ def fwd(robobj, thetalist):
     return robobj.coordmat
 
 
+#create rotation matrix for each joint
 def rotation_matrix(robobj, thetalist):
 
     i = 0
@@ -49,88 +53,24 @@ def rotation_matrix(robobj, thetalist):
     return rotmat
 
 
-def rotation_matrix_dif(robobj, thetalist):
+def inv(robobj, coord):
 
-    i = 0
-    rotmatdif = np.zeros((robobj.jointno, 3, 3))
+    [x, y, z] = coord
+    thetalist = np.zeros(3)
 
-    while i < robobj.jointno:
-        rotmatdif[i][0] = [-math.sin(thetalist[i]) + (robobj.jointaxismat[i][0]**2)*(math.sin(thetalist[i])),
-                        ((robobj.jointaxismat[i][0]*robobj.jointaxismat[i][1])*(math.sin(thetalist[i])) -
-                         (robobj.jointaxismat[i][2]*math.cos(thetalist[i]))),
-                        ((robobj.jointaxismat[i][0]*robobj.jointaxismat[i][2])*(math.sin(thetalist[i])) +
-                         (robobj.jointaxismat[i][1]*math.cos(thetalist[i])))]
-        rotmatdif[i][1] = [((robobj.jointaxismat[i][0]*robobj.jointaxismat[i][1])*(math.sin(thetalist[i])) +
-                            (robobj.jointaxismat[i][2]*math.cos(thetalist[i]))),
-                        -math.sin(thetalist[i]) + (robobj.jointaxismat[i][1]**2)*(math.sin(thetalist[i])),
-                        ((robobj.jointaxismat[i][1]*robobj.jointaxismat[i][2])*(math.sin(thetalist[i])) -
-                         (robobj.jointaxismat[i][0]*math.cos(thetalist[i])))]
-        rotmatdif[i][2] = [((robobj.jointaxismat[i][0]*robobj.jointaxismat[i][2])*(math.sin(thetalist[i])) -
-                            (robobj.jointaxismat[i][1]*math.cos(thetalist[i]))),
-                        ((robobj.jointaxismat[i][1]*robobj.jointaxismat[i][2])*(math.sin(thetalist[i])) +
-                         (robobj.jointaxismat[i][0]*math.cos(thetalist[i]))),
-                        math.sin(thetalist[i]) + (robobj.jointaxismat[i][2]**2)*(1 - math.sin(thetalist[i]))]
-        i = i + 1
+    if(x == 0):
+        thetalist[0] = 0
+    else:
+        thetalist[0] = math.atan(y / x)
 
-    return rotmatdif
+    d = math.sqrt((z - robobj.lenmat[0][2])**2 + x**2 + y**2)
 
 
-def jacobian(robobj, thetalist):
 
-    rotmat = rotation_matrix(robobj, thetalist)
-    rotmatdif = rotation_matrix_dif(robobj, thetalist)
+    thetalist[2] = pi - math.acos((robobj.lenmat[1][2]**2 + robobj.lenmat[2][2]**2 - d**2) /
+                                    (2*robobj.lenmat[1][2]*robobj.lenmat[2][2]))
 
-    jparts = np.zeros((robobj.jointno, 3))
-    jacob = np.zeros((robobj.jointno, 3))
+    theta = math.asin(robobj.lenmat[2][2] * math.sin(thetalist[2]) / d)
+    thetalist[1] = math.acos((z - robobj.lenmat[0][2]) / d) - theta
 
-    i = 0
-    while i < robobj.jointno:
-        jparts[i] = robobj.initcoordmat[i+1]
-        i = i + 1
-
-    i = 0
-    while i < robobj.jointno:
-
-        j = 0
-        while j < robobj.jointno:
-            k = j
-            while k > -1:
-                jparts[j] = jparts[j].__sub__(robobj.initcoordmat[k])
-                if k == i:
-                    jparts[j] = rotmatdif[k].dot(jparts[j])
-                else:
-                    jparts[j] = rotmat[k].dot(jparts[j])
-                jparts[j] = jparts[j].__add__(robobj.initcoordmat[k])
-
-                k = k - 1
-            j = j + 1
-
-        jacob = jacob.__add__(jparts)
-        i = i + 1
-
-    return jacob[robobj.jointno - 1]
-
-
-def inv(robobj, pos, thetaguess, er):
-    found = 0
-    ind = robobj.jointno
-    i = 0
-
-    while found == 0:
-        g1 = fwd(robobj,thetaguess)
-        jac = jacobian(robobj,thetaguess)
-
-        print(g1[ind])
-
-        if math.sqrt( (g1[ind][0] - pos[0])**2 + (g1[ind][1] - pos[1])**2 + (g1[ind][2] - pos[2])**2) < er:
-            found = 1
-        elif i > 10:
-            found = 2
-        else:
-            thetaguess[0] = thetaguess[0] + (g1[ind][0] - pos[0])/jac[0]
-            thetaguess[1] = thetaguess[1] + (g1[ind][1] - pos[1])/jac[1]
-            thetaguess[2] = thetaguess[2] + (g1[ind][2] - pos[2])/jac[2]
-        print(i)
-        i = i + 1
-
-    return thetaguess
+    return thetalist
