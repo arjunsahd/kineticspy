1970c855a986e6641b4fdd130e0e3062bb7b7584
Kaneki99 <arjunsahdev13@gmail.com>
2019-08-09 17:51:39 +0530

    Packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simulpy',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/Kaneki99/simulpy',
+    license='',
+    author='arjun',
+    author_email='arjunsahdev13@gmail.com',
+    description='Simulation tool for Robot Arm'
+)
