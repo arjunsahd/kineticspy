628cecb2f7152deff7579c608e2637c09acd96ea
Kaneki99 <arjunsahdev13@gmail.com>
2019-08-09 20:44:01 +0530

    Packaging

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="simulpy-Kaneki99",
+    name="simulpy",
     version="0.0.1",
     author="Arjun Sahdev",
     author_email="arjunsahdev13@gmail.com",
